b153c43101f177f05ed6e1955b93ca25a5f23e33
mycahdt <79180400+mycahdt@users.noreply.github.com>
2023-08-03 13:37:40 -0400

    Add files via upload

---
+++ proj8testing.py
@@ -0,0 +1,17 @@
+mylength = len("Pardon me for not rising.") + len("1946-1957\n1980-1981\n1985-1986\netc.")
+mylen = len("Shh... The maestro is decomposing.")
+mylenlen = len("YOUR NAME HERE.") 
+
+total = mylength + mylen + mylenlen
+print(total)
+501 - 12
+
+
+'''
+grave = "Hello there\nmommy hi"
+message = grave.split()
+mytotal = 0
+for word in message:
+   mytotal = mytotal + len(word) 
+print(mytotal)
+'''
